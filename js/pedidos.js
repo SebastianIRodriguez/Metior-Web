2e4217773a7d010e9c92189af9c7c67f2ff7b928
Tomas Pitinari <tomy.pitinari@gmail.com>
2019-09-15 23:06:57 -0300

    Se generalizo el footer, se mejoro el estilo de las paginas principales y se hizo un diseno mas fachero para la lista de pedidos

--- js/pedidos.js
+++ js/pedidos.js
@@ -5,11 +5,78 @@ function loadListaPedidos() {
         var pedidos = JSON.parse(data)
 
         var container = document.getElementById('responsecontainer')
+
+        var id = document.createElement("div")
+        id.setAttribute("class", "text-light flex-center")
+        id.setAttribute("style","width:5%;border-style: solid;border-width: 1px;")
+        id.innerHTML = "ID"
+
+        var cant = document.createElement("div")
+        cant.setAttribute("class", "text-light flex-center")
+        cant.setAttribute("style","width:5%;border-style: solid;border-width: 1px;")
+        cant.innerHTML = "Cant."
+
+        var nombre = document.createElement("div")
+        nombre.setAttribute("class", "text-light flex-center")
+        nombre.setAttribute("style","width:30%;border-style: solid;border-width: 1px;")
+        nombre.innerHTML = "Nombre"
+
+        var apellido = document.createElement("div")
+        apellido.setAttribute("class", "text-light flex-center")
+        apellido.setAttribute("style","width:30%;border-style: solid;border-width: 1px;")
+        apellido.innerHTML = "Apellido"
+
+        var direccion = document.createElement("div")
+        direccion.setAttribute("class", "text-light flex-center")
+        direccion.setAttribute("style","width:30%;border-style: solid;border-width: 1px;")
+        direccion.innerHTML = "Direccion"
+
+        var guia = document.createElement("div")
+        guia.setAttribute("style","display:flex;flex-direction:row;width:100%")
+
+        guia.appendChild(id)
+        guia.appendChild(cant)
+        guia.appendChild(nombre)
+        guia.appendChild(apellido)
+        guia.appendChild(direccion)
+
+        container.appendChild(guia)
+
         for (var pedido of pedidos) {
-            var para = document.createElement("p")
-            var node = document.createTextNode(JSON.stringify(pedido))
-            para.appendChild(node)
-            para.setAttribute("class", "text-light")
+
+            var id = document.createElement("div")
+            id.setAttribute("class", "text-light flex-center")
+            id.setAttribute("style","width:5%;border-style: solid;border-width: 1px;")
+            id.innerHTML = pedido.id
+
+            var cant = document.createElement("div")
+            cant.setAttribute("class", "text-light flex-center")
+            cant.setAttribute("style","width:5%;border-style: solid;border-width: 1px;")
+            cant.innerHTML = pedido.cant
+
+            var nombre = document.createElement("div")
+            nombre.setAttribute("class", "text-light flex-center")
+            nombre.setAttribute("style","width:30%;border-style: solid;border-width: 1px;")
+            nombre.innerHTML = pedido.nombre
+
+            var apellido = document.createElement("div")
+            apellido.setAttribute("class", "text-light flex-center")
+            apellido.setAttribute("style","width:30%;border-style: solid;border-width: 1px;")
+            apellido.innerHTML = pedido.apellido
+
+            var direccion = document.createElement("div")
+            direccion.setAttribute("class", "text-light flex-center")
+            direccion.setAttribute("style","width:30%;border-style: solid;border-width: 1px;")
+            direccion.innerHTML = pedido.direccion
+
+            var para = document.createElement("div")
+            para.setAttribute("style","display:flex;flex-direction:row;width:100%")
+
+            para.appendChild(id)
+            para.appendChild(cant)
+            para.appendChild(nombre)
+            para.appendChild(apellido)
+            para.appendChild(direccion)
 
             container.appendChild(para)
         }
