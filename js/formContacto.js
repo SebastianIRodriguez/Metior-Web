f322cc6c509399bd298424fd79284b77b3e95a78
SebastianIRodriguez <48632951+SebastianIRodriguez@users.noreply.github.com>
2019-10-27 11:41:56 -0300

    Agregada funcionalidad de enviar mails en contacto

--- js/formContacto.js
+++ js/formContacto.js
@@ -1,38 +1,41 @@
 function enviarFormContacto() {
 
     //objeto nuevo al que cargamos los datos que el usuario puso en los inputs a los que identificamos por su ID
-    /*var obj = {};
-    obj.nombre = document.getElementById('nombre_usuario').value;
-    obj.apellido = document.getElementById('ape_usuario').value;
-    obj.direccion = document.getElementById('direccion_usuario').value;
+    let obj = {
+        nombre: document.getElementById('nombre_usuario').value,
+        apellido: document.getElementById('ape_usuario').value,
+        mail: document.getElementById('direccion_usuario').value,
+        mensaje: "Hola amiguito"
+    }
 
     // convertimos el objeto a formato JSON
-    var parametros = JSON.stringify(obj);
+    let parametros = JSON.stringify(obj);
 
-    $.post('./php/grabar.php', "x=" + parametros,
+    $.post('./php/mail.php', "x=" + parametros,
         function(data, status, xhr) {
-
+            console.log(data)
 
             document.getElementById('nombre_usuario').value = '';
             document.getElementById('ape_usuario').value = '';
             document.getElementById('direccion_usuario').value = '';
 
         }
-    );*/
+    );
 }
 
 function enviarFormulario() {
-    var obj = {};
-    obj.nombre = document.getElementById('firstName').value;
-    obj.apellido = document.getElementById('lastName').value;
-    obj.email = document.getElementById('email').value;
-    obj.direccion = document.getElementById('address').value;
-    obj.pais = document.getElementById('country').value;
-    obj.provincia = document.getElementById('state').value;
-    obj.cod_post = document.getElementById('zip').value;
+    let obj = {
+        nombre: document.getElementById('firstName').value,
+        apellido: document.getElementById('lastName').value,
+        email: document.getElementById('email').value,
+        direccion: document.getElementById('address').value,
+        pais: document.getElementById('country').value,
+        provincia: document.getElementById('state').value,
+        cod_post: document.getElementById('zip').value
+    }
 
     // convertimos el objeto a formato JSON
-    var parametros = JSON.stringify(obj);
+    let parametros = JSON.stringify(obj);
 
     //parametros = '{"nombre":"Seba","apellido":"R","email":"seba.rodriguez.994@gmail.com","direccion":"4","pais":"Argentina","provincia":"California","cod_post":"646"}'
 
--- php/mail.php
+++ php/mail.php
@@ -1,15 +1,43 @@
 <?php
-  $nombre = $_POST['nombre'];
-  $mail = $_POST['mail'];
-  $mensaje = $_POST['mensaje'];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
-  if(mail('tomy-pitinari@hotmail.com', $mail, $mensaje)){
-    echo "<script type=\"text/javascript\">window.alert('El mail fue enviado con exito.');
-    window.location.href = 'contacto.html';</script>";
-    exit;
-  }else{
-    echo "<script type=\"text/javascript\">window.alert('El mail no se pudo enviar.');
-    window.location.href = 'contacto.html';</script>";
-    exit;
-  }
-?>
+require 'mailer/Exception.php';
+require 'mailer/PHPMailer.php';
+require 'mailer/SMTP.php';
+
+$obj = json_decode($_POST['x'], false);
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+
+$mail->Debugoutput = 'html';
+
+$mail->isSMTP();
+
+$mail->SMTPDebug = 2;
+
+$mail->Host = 'tls://smtp.gmail.com:587';
+
+$mail->SMTPAuth = true;
+
+$mail->SMTPAutoTLS = false;
+
+$mail->Username = 'webmetior@gmail.com';
+$mail->Password = 'peifndoemujiiknt';
+
+$mail->setFrom($obj->mail, $obj->nombre . $obj->apellido);
+
+$mail->addReplyTo('webmetior@gmail.com');
+
+$mail->addAddress('webmetior@gmail.com');
+
+$mail->Subject = 'Mail de contacto';
+$mail->Body = $obj->mensaje;
+
+if (!$mail->send()) {
+    echo 'Mailer Error: '. $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
+}
