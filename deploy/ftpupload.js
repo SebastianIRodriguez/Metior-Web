93022cbd21f38f1b64e716474af70fd7a142a5de
Tomas Pitinari <48494596+Pitinari@users.noreply.github.com>
2019-10-29 15:48:11 -0300

    Update ftpupload.js

--- deploy/ftpupload.js
+++ deploy/ftpupload.js
@@ -34,7 +34,7 @@ function getConfiguration() {
         password: process.env.ftp_password,
         localRoot: process.cwd(),
         remoteRoot: process.env.ftp_remotePath,
-        exclude: ["dist/**/*.map", "node_modules/**", "node_modules/**/.*"]
-        //deleteRemote: false
+        exclude: ["dist/**/*.map", "node_modules/**", "node_modules/**/.*",".*",".*/*"],
+        deleteRemote: true
     };
 }
